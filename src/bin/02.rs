46c8a38a58d46ebf260089fedee0345b05d34a56
Salim Khan <Khan.salim.ali@gmail.com>
2023-07-30 21:08:35 -0400

    Solved part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -23,12 +23,32 @@ fn PointsFromShape(Shape: &str) -> u32 {
         "X" => result = 1,
         "Y" => result = 2,
         "Z" => result = 3,
+        "A" => result = 1,
+        "B" => result = 2,
+        "C" => result = 3,
         _ => print!("Not valid Shape"),
     }
     result
 }
 
-pub fn parse_input(input: &str, score: &mut u32) {
+fn PointsFromExpectedResult(Shapes: Vec<&str>) -> u32 {
+    let rounds = HashMap::from([
+        ("AX".to_string(), 3),
+        ("AY".to_string(), 4),
+        ("AZ".to_string(), 8),
+        ("BX".to_string(), 1),
+        ("BY".to_string(), 5),
+        ("BZ".to_string(), 9),
+        ("CX".to_string(), 2),
+        ("CY".to_string(), 6),
+        ("CZ".to_string(), 7),
+    ]);
+
+    let ThisRound: String = Shapes.join("");
+    rounds[&ThisRound]
+}
+
+pub fn parse_input1(input: &str, score: &mut u32) {
     for line in input.lines() {
         let mut RoundScore: u32 = 0;
         if line.is_empty() {
@@ -37,21 +57,32 @@ pub fn parse_input(input: &str, score: &mut u32) {
             RoundScore += PointsFromShape(Shapes[1]);
             RoundScore += PointsFromResult(Shapes);
         }
-        // print!("{}", RoundScore);
-        // print!("{}", score);
         *score += RoundScore;
-        // print!("{}", score);
+    }
+}
+
+pub fn parse_input2(input: &str, score: &mut u32) {
+    for line in input.lines() {
+        let mut RoundScore: u32 = 0;
+        if line.is_empty() {
+        } else {
+            let Shapes: Vec<&str> = line.split(' ').collect();
+            RoundScore += PointsFromExpectedResult(Shapes);
+        }
+        *score += RoundScore;
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let mut result: u32 = 0;
-    parse_input(input, &mut result);
+    parse_input1(input, &mut result);
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut result: u32 = 0;
+    parse_input2(input, &mut result);
+    Some(result)
 }
 
 fn main() {
@@ -67,12 +98,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_one(&input), Some(63));
+        assert_eq!(part_one(&input), Some(15));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
